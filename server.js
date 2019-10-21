ab010ff32c75c0a907d84f08f0d6c0ed334e5919
bearados <jessados1987@gmail.com>
2019-10-21 12:15:40 +0900

    make it better

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const port = process.env.PORT || 5000;
+
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  
+  msg = "having fun with data";
+  res.send({ express: msg });
+});
+
+app.get('/getdata', (req, res) => {
+  graphdata = {
+    nodes: [ 
+        { 
+          "id": "1"
+        },
+        { 
+          "id": "2"
+        },
+        {
+          "id": "3"
+        },
+        {
+          "id": "4"
+        }
+    ],
+    links: [
+        {
+            "source": "1",
+            "target": "2"
+        },
+        {
+          "source": "2",
+          "target": "3"
+        },
+        {
+          "source": "3",
+          "target": "4"
+        }
+    ]
+}
+  res.send({express : graphdata });
+});
+
+
+
+app.get('/getQuiz', (req, res) => {
+  res.send("success")
+});
+
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
