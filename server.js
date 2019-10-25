4c6081b04b0d913b9ef6339b03eace919ff304aa
bearados <jessados1987@gmail.com>
2019-10-25 11:25:03 +0900

    ignore node_modules

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.get('/', (req, res) => {
-  res.render("index.html");
+  res.send("get success");
 });
 
 // create a GET route
