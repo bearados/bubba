2303ac492eb64046bcc8b421a6f36a56f8a8c612
bearados <jessados1987@gmail.com>
2019-10-25 15:23:44 +0900

    ignore node_modules

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -8,8 +8,8 @@ const Navigation = () => {
         <Navbar bg="dark" variant="dark" expand="lg">
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="#scores">Scores</Nav.Link>
-          </Nav>
+          <Nav.Link href="/scores">Scores</Nav.Link>
+        </Nav>
         </Navbar>
        </div>
     );
