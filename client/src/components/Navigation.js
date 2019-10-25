4a612c5aff1fe1a250e98ef49e05b3598c2ee722
bearados <jessados1987@gmail.com>
2019-10-25 15:07:21 +0900

    ignore node_modules

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,13 +1,16 @@
 import React from 'react';
  
-import { NavLink } from 'react-router-dom';
+import { Navbar, Nav } from 'react-router-dom';
  
 const Navigation = () => {
     return (
        <div>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+        <Navbar bg="dark" variant="dark" expand="lg">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/scores">Scores</Nav.Link>
+          </Nav>
+        </Navbar>
        </div>
     );
 }
