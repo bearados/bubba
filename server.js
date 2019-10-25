97e34e56ae63c6e8affee7bb71fdd5197e95f17e
bearados <jessados1987@gmail.com>
2019-10-25 12:32:17 +0900

    ignore node_modules

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 5000;
 var path = require('path');
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 
 
