ff9fe28dc5a7119e11db77eea3dc30dba4b19553
bearados <jessados1987@gmail.com>
2019-10-25 17:10:04 +0900

    ignore node_modules

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ const port = process.env.PORT || 5000;
 var path = require('path');
 app.use(express.static(path.join(__dirname, '/client/build')));
 
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
