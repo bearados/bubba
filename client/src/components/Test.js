9ee237b546e4a4d4c648280cb7b2bff1437fd457
bearados <jessados1987@gmail.com>
2019-10-30 20:40:44 +0900

    sub Quiz

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import { Tabs, Tab } from 'react-bootstrap';
 import QuizA from './QuizA'
+import QuizS from './QuizS'
 
 class Test extends Component {
     render(){
@@ -11,8 +12,9 @@ class Test extends Component {
                 <h1>Addition Quiz</h1>
                 <QuizA/>
             </Tab>
-            <Tab eventKey="sub" title="Subtraction Test">
-                <h2>Try these 20 subtraction problems!</h2>
+            <Tab eventKey="sub" title="Subtraction Quiz">
+                <h1>Subtraction Quiz</h1>
+                <QuizS/>
             </Tab>
         </Tabs>
         );
--- server.js
+++ server.js
@@ -6,6 +6,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 5000;
+/*
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+client.connect();
+*/
+/*client.query('CREATE TABLE if not exists user_list ( username varchar(255),password varchar(255))');
+client.query('CREATE TABLE user_scores (username varchar,score int, date_time date, FOREIGN KEY (username) REFERENCES user_list(username))', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});*/
+
 var path = require('path');
 app.use(express.static(path.join(__dirname, '/client/build')));
 
