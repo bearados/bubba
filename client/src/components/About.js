0c2d090f2de34164a9c7aa8da48cfd2a6e12b2e1
bearados <jessados1987@gmail.com>
2019-11-23 10:55:42 +0900

    d3

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,16 +6,15 @@ const About = () => {
        <div>
         <Jumbotron>
           <h1>About BubbaTest</h1>
-          <p>This is a simple React/Node web-app made to help kids improve their math skills.</p>
-          <p>The graph scores page was built using react-d3-graph and Heroku Postgres</p>
+          <p>This is a simple React/Node web-app made to help kids improve their math skills. The graph on the Scores page was built using react-d3-graph and Heroku Postgres.</p>
           
           <ListGroup variant="flush"><Row><Col>Project Links:</Col><Col>Other Projects by Jessica Adams:</Col></Row> 
           <Row>
             <Col><ListGroup.Item sz="sm"><a href="https://github.com/bearados/bubba">View this project on github</a></ListGroup.Item></Col>
            
             
-            <Col><ListGroup.Item><a href="https://atlascrawler.herokuapp.com">Web Crawler (Group Project)</a></ListGroup.Item>
-            <ListGroup.Item><a href="https://midwayfairies.herokuapp.com">Midway Fairies site</a></ListGroup.Item></Col>
+            <Col><ListGroup.Item><a href="https://atlascrawler.herokuapp.com">Web Crawler (Group Project)</a></ListGroup.Item></Col>
+            
             </Row>
             </ListGroup>
             <br/>
