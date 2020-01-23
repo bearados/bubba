ef22beb06fae5857ad9e8d15361b494130aef972
bearados <jessados1987@gmail.com>
2020-01-23 14:48:24 +0900

    change quiz to 15 problems

--- client/src/components/QuizA.js
+++ client/src/components/QuizA.js
@@ -87,7 +87,8 @@ class QuizA extends Component {
             
            <Jumbotron>
                {this.state.probList.map(co=>(<Form>
-                <Row className="justify-content-md-center"><Col>{++i}. {co} </Col><Col><Form.Control size="sm" type="number" name = {i} onChange={this.handleChange.bind(this)} placeholder="0" /></Col></Row>
+                <Row className="justify-content-md-center"><Col>{++i}. {co} </Col><Col><Form.Control size="sm" type="number" name = {i} onChange={this.handleChange.bind(this)} placeholder="0" /></Col>
+                <Col>{++i}. {co} </Col><Col><Form.Control size="sm" type="number" name = {i} onChange={this.handleChange.bind(this)} placeholder="0" /></Col></Row>
                    <br/>   
                 </Form>
                 ))} <Row className="justify-content-md-center"><Button onClick= {this.getScore.bind(this)} variant="primary">Submit Answers</Button>
