1b0221f68b57525e0d220691f68df1b6d7688f81
bearados <jessados1987@gmail.com>
2020-10-18 11:05:19 -0700

    updated about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -8,15 +8,11 @@ const About = () => {
           <h1>About BubbaTest</h1>
           <p>BubbaTest is a site for practicing math problems. This app was built using Heroku Postgres, React, and Node.</p>
           
-          <ListGroup variant="flush"><Row><Col>Project Links:</Col><Col>Other Projects by Jessica Adams:</Col></Row> 
+          <Row><Col>Project Links:</Col><Col>Other Projects by Jessica Adams:</Col></Row> 
           <Row>
-            <Col><ListGroup.Item sz="sm"><a href="https://github.com/bearados/bubba">View this project on github</a></ListGroup.Item></Col>
-           
+          <a href="https://github.com/bearados/bubba">View this project on github</a>  
+          </Row>
             
-            <Col><ListGroup.Item><a href="https://atlascrawler.herokuapp.com">Web Crawler (Group Project)</a></ListGroup.Item></Col>
-            
-            </Row>
-            </ListGroup>
             <br/>
             <p>Author: Jessica Adams <br/>
             <a href="https://www.linkedin.com/in/jessica-adams-36a98762/">Find me on linkedin!</a></p>
