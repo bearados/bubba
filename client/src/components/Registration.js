9145dee8fb28154bf1f31b573a1e6460c5fdc5b1
bearados <jessados1987@gmail.com>
2023-10-28 23:01:50 -0700

    registration

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -7,29 +7,29 @@ const Registration = () => {
         <Form>
             <h1>Register</h1><br/>
             <Row>
-                <Form.Group as={Col} md="4" controlId="validationCustom01">
+                <Form.Group as={Col} controlId="validationCustom01">
                     <Form.Label>First name</Form.Label>
-                    <Form.Control required type="text" placeholder="First name" defaultValue="Mark"/>
+                    <Form.Control required type="text" placeholder="First name"/>
                     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
-                <Form.Group as={Col} md="4" controlId="validationCustom02">
+                <Form.Group as={Col} controlId="validationCustom02">
                     <Form.Label>Last name</Form.Label>
-                    <Form.Control required type="text" placeholder="Last name" defaultValue="Otto"/>
+                    <Form.Control required type="text" placeholder="Last name"/>
                     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
             </Row>
             <Row>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Group cas={Col} controlId="exampleForm.ControlInput1">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="name@example.com" />
                 </Form.Group>
             </Row>
             <Row>
-                <Form.Group as={Col} md="4"className="mb-3" controlId="formPlaintextPassword">
+                <Form.Group as={Col} controlId="formPlaintextPassword">
                     <Form.Label for="password1" >Password</Form.Label>
                     <Form.Control id="password1" type="password" placeholder="Password" />
                 </Form.Group>
-                <Form.Group as={Col} md="4"className="mb-3" controlId="formPlaintextPassword">
+                <Form.Group as={Col} controlId="formPlaintextPassword">
                     <Form.Label for="password2" >Re-enter Password</Form.Label>
                     <Form.Control id="password2" type="password" placeholder="Password" />
                 </Form.Group>
